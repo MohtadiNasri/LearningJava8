ed28669460c0203d8ef995104c4a5a43ea70c3cb
nasrimohtadi@gmail.com <Awkward@009>
2020-05-02 02:15:24 +0200

    Removing duplicates from a list of integers

--- src/ImperativeVSDeclarativeProgramming.java
+++ src/ImperativeVSDeclarativeProgramming.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ImperativeVSDeclarativeProgramming {
@@ -13,22 +17,36 @@ public class ImperativeVSDeclarativeProgramming {
         for(int i=0;i<=100;i++){
             sum+=i;
         }
-        System.out.println("Sum using the imperative approach! "+ sum);
+        //System.out.println("Sum using the imperative approach! "+ sum);
         /*
          * Declarative Style of programming
          * */
-        int resultDeclative = IntStream.rangeClosed(0,100).sum();
-        System.out.println("Sum using the declarative approach! "+ resultDeclative);
+        int resultDeclative = IntStream
+                .rangeClosed(0,100)
+                .sum();
+        //System.out.println("Sum using the declarative approach! "+ resultDeclative);
 
 
         //second Example - Removing duplicates from a list of integers
+        List<Integer> duplicateList = Arrays.asList(1,2,2,3,4,5,6,7,7,7,7,8,8,8,9,9,9,9);
         /*
          * Imperative Style of programming
          * */
-         
+         List<Integer> uniqueList = new ArrayList<>();
+         for(Integer integer : duplicateList){
+             if(!uniqueList.contains(integer)){
+                 uniqueList.add(integer);
+             }
+         }
+         System.out.println("First unique list : "+uniqueList);
         /*
          * Declarative Style of programming
          * */
+        List<Integer> uniquelist = duplicateList.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("Second unique list : "+uniquelist);
+
 
     }
 }
