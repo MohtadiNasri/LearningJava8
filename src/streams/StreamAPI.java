56ecc3abf6b7c34dfec8c551c20f17357095b842
nasrimohtadi <mohtadi-nasri@forcus-corporation.com>
2020-12-02 13:33:36 +0100

    Map

--- src/streams/StreamAPI.java
+++ src/streams/StreamAPI.java
@@ -3,6 +3,7 @@ package streams;
 import data.Person;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -24,7 +25,7 @@ public class StreamAPI {
         //Correct Way
         streamStudent.filter(p -> p.getAge()>20).forEach(p-> System.out.println(p));
 
-        System.out.println("-----------------------------------------------------");
+        System.out.println("-------------------FILTER---------------------------");
 
         //Working with predicate
         Stream<String> streamOfNumber = Stream.of("one","two","three","four","five");
@@ -34,6 +35,19 @@ public class StreamAPI {
         streamOfNumber.filter(filterUsingPredicate)
                 .forEach(n-> System.out.println(n));
 
+        System.out.println("---------------------MAP---------------------");
+
+        //Map
+        List<Integer> list1 = Arrays.asList(1,2,3,4,5,6,7);
+        List<Integer> list2 = Arrays.asList(2,4,6);
+        List<Integer> list3 = Arrays.asList(3,5,7);
+
+        List<List<Integer>> finalList = Arrays.asList(list1,list2,list3);
+
+        finalList.stream()
+                .peek(l->System.out.println(l))
+                .map(l->l.size())
+                .forEach(System.out::println);
 
     }
 }
