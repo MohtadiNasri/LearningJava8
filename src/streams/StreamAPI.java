a85871dd130381a96aab0da3bd1e6977d6f83b9c
nasrimohtadi <mohtadi-nasri@forcus-corporation.com>
2020-12-02 12:58:26 +0100

    Best Practice of Streams Predicate and Consumer

--- src/streams/StreamAPI.java
+++ src/streams/StreamAPI.java
@@ -4,6 +4,7 @@ import data.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamAPI {
@@ -16,9 +17,23 @@ public class StreamAPI {
 
 
         //Consumer
-        streamStudent.forEach(p-> System.out.println(p));
-        //Predicate
-        streamStudent.filter(p -> p.getAge()>20);
+        //streamStudent.forEach(p-> System.out.println(p));
+        //Predicate : the line below will rais en exception as we are not allowed to use stream twice
+        //streamStudent.filter(p -> p.getAge()>20);
+
+        //Correct Way
+        streamStudent.filter(p -> p.getAge()>20).forEach(p-> System.out.println(p));
+
+        System.out.println("-----------------------------------------------------");
+
+        //Working with predicate
+        Stream<String> streamOfNumber = Stream.of("one","two","three","four","five");
+        //first Predicate
+        Predicate<String> filterUsingPredicate = s->s.length()>3;
+
+        streamOfNumber.filter(filterUsingPredicate)
+                .forEach(n-> System.out.println(n));
+
 
     }
 }
